d3758ec69287bab593049467f8d5e4e621597f49
abd1bayev <jasurabdibayev0@gmail.com>
2023-04-06 09:53:22 +0500

    update barcode

--- products/models.py
+++ products/models.py
@@ -3,22 +3,23 @@ import barcode
 from barcode.writer import ImageWriter
 from io import BytesIO
 from django.core.files import File
-# Create your models here.
+
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
     barcode = models.ImageField(upload_to='images/', blank=True)
-    country_id = models.CharField(max_length=1, null=True)
-    manufacturer_id = models.CharField(max_length=6, null=True)
-    product_id = models.CharField(max_length=5, null=True)
+    country_code = models.CharField(max_length=3, null=True)
+    manufacturer_code = models.CharField(max_length=5, null=True)
+    product_code = models.CharField(max_length=5, null=True)
 
     def __str__(self):
         return str(self.name)
 
     def save(self, *args, **kwargs):
-        EAN = barcode.get_barcode_class('ean13')
-        ean = EAN(f'{self.country_id}{self.manufacturer_id}{self.product_id}', writer=ImageWriter())
-        buffer = BytesIO()
-        ean.write(buffer)
-        self.barcode.save(f'{self.name}.png', File(buffer), save=False)
+        if not self.pk:
+            EAN = barcode.get_barcode_class('ean13')
+            ean = EAN(f'0{self.country_code}{self.manufacturer_code}{self.product_code}', writer=ImageWriter())
+            with BytesIO() as buffer:
+                ean.write(buffer)
+                self.barcode.save(f'{self.name}.png', File(buffer), save=False)
         return super().save(*args, **kwargs)
